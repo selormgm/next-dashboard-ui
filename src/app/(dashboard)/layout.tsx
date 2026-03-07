3f742efc3418f4010ff1922269cd0537836fdfb1
Selorm Gawu <128249946+selormgm@users.noreply.github.com>
2026-03-07 11:55:08 +0000

    dd

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,7 +1,33 @@
+import Link from "next/link";
+import Image from "next/image";
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div>test{children}</div>;
+  return (
+    <div className="h-screen flex">
+      {/*LEFT */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+        <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
+          <Image
+            src="/logo.png"
+            alt="logo"
+            width={32}
+            height={32}
+          />
+          <span className="hidden lg:block">SchoolLama</span>
+        </Link>
+        <Menu/>
+      </div>
+
+      {/*RIGHT */}
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#f7f8fa] overflow-scroll">
+        <Navbar/>
+      </div>
+    </div>
+  );
 }
