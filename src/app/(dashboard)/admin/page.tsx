c2dcb42d53f8a9b13ea000b0af9d3f69c0e551bd
Selorm Gawu <128249946+selormgm@users.noreply.github.com>
2026-03-08 22:45:19 +0000

    kk

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,10 +1,23 @@
+import UserCard from "@/components/UserCard"
+
 const AdminPage = () => {
   return (
-    <div>
-      <h1>Admin Dashboard</h1>
-      <p>Welcome to the admin panel!</p>
-    </div>
-  );
-};
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/*Left*/}
+      <div className="w-full lg:w-2/3">
+      {/* UserCards */}
+      <div className="flex gap-4 justify-between">
+        <UserCard type="admin" />
+        <UserCard type="student" />
+        <UserCard type="teacher" />
+        <UserCard type="stuff" />
+      </div>
 
-export default AdminPage;
+      </div>
+      {/*Right*/}
+      <div className="w-full lg:w-1/3">right</div>
+    </div>
+  )
+}
+
+export default AdminPage
---
+++ src/components/UserCard.tsx
@@ -0,0 +1,9 @@
+const UserCard = ({type}:{type:string}) => {
+  return (
+    <div className="rounded-2xl odd:bg-lamaPurple even:bg-lamaYellow p-4">
+      test
+    </div>
+  )
+}
+
+export default UserCard
